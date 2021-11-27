66c6baf289ac3cb77f214d61e0d1c8a6449978a0
okmaidpansy <94445523+Okmaid102@users.noreply.github.com>
2021-11-27 20:41:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "🥰🥰🥰",
+    introTitle: 'chào cậu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
